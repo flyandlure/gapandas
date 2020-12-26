893ae03c6ee959337e07505ebf711c981dbf1a2c
Matt Clarke <matt@flyandlure.org>
2020-12-26 10:55:55 +0000

    Added verbose argument to get_service()

--- gapandas/connect.py
+++ gapandas/connect.py
@@ -14,18 +14,31 @@ from oauth2client.service_account import ServiceAccountCredentials
 socket.setdefaulttimeout(500)
 
 
-def get_service(keyfile_path):
+def get_service(keyfile_path, verbose=False):
     """Return a service to communicate with the Google Analytics API
        using settings from the configuration file.
 
     :param keyfile_path - Path to client_secrets.json
+    :param verbose: Set to True to see messages
     """
+
+    if verbose:
+        print('Attempting connection')
 
     try:
         credentials = ServiceAccountCredentials.from_json_keyfile_name(keyfile_path,
                                                                        scopes="https://www.googleapis.com/auth/analytics.readonly")
         service = build("analytics", "v3", credentials=credentials)
+
+        if verbose:
+            print('Connected successfully')
+
         return service
 
     except Exception as e:
+
+        if verbose:
+            print('Connection failed: \
+            Check your client_secrets.json and ensure the email is in your Google Analytics account.')
+
         return e
